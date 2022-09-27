48e1b3f16c3e5a9ef98025605fda1622aeacfe44
Luna Nova <git@lunnova.dev>
2022-09-26 22:10:57 -0700

    allow loading directly if missing top level state_dict

--- ldm/models/autoencoder.py
+++ ldm/models/autoencoder.py
@@ -89,7 +89,9 @@ class VQModel(pl.LightningModule):
                     print(f'{context}: Restored training weights')
 
     def init_from_ckpt(self, path, ignore_keys=list()):
-        sd = torch.load(path, map_location='cpu')['state_dict']
+        sd = torch.load(path, map_location='cpu')
+        if 'state_dict' in sd:
+            sd = sd['state_dict']
         keys = list(sd.keys())
         for k in keys:
             for ik in ignore_keys:
@@ -399,7 +401,9 @@ class AutoencoderKL(pl.LightningModule):
             self.init_from_ckpt(ckpt_path, ignore_keys=ignore_keys)
 
     def init_from_ckpt(self, path, ignore_keys=list()):
-        sd = torch.load(path, map_location='cpu')['state_dict']
+        sd = torch.load(path, map_location='cpu')
+        if 'state_dict' in sd:
+            sd = sd['state_dict']
         keys = list(sd.keys())
         for k in keys:
             for ik in ignore_keys:
